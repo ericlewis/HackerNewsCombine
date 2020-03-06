a9697451a11cb3e722a9d78f30625e5957689863
Eric Lewis <ericlewis777@gmail.com>
2020-03-06 13:15:25 -0500

    a

--- Sources/HackerNewsCombine/Item.swift
+++ Sources/HackerNewsCombine/Item.swift
@@ -34,10 +34,32 @@ public struct Item: ItemProtocol {
     public var title: String?
     public var parts: [Int]?
     public var descendants: Int?
+    
+    enum CodingKeys : CodingKey {
+        case id, deleted, type, by, time, text, dead, parent, poll, kids, url, score, title, parts, descendants
+    }
 }
 
 extension Item: Identifiable {
 }
 
 extension Item: Decodable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        deleted = try container.decodeIfPresent(Bool.self, forKey: .deleted)
+        type = try container.decodeIfPresent(ItemType.self, forKey: .type)
+        by = try container.decodeIfPresent(String.self, forKey: .by)
+        time = try container.decodeIfPresent(Date.self, forKey: .time)
+        text = try container.decodeIfPresent(String.self, forKey: .text)
+        dead = try container.decodeIfPresent(Bool.self, forKey: .dead)
+        parent = try container.decodeIfPresent(Int.self, forKey: .parent)
+        poll = try container.decodeIfPresent(Int.self, forKey: .poll)
+        kids = try container.decodeIfPresent([Int].self, forKey: .kids)
+        url = try? container.decodeIfPresent(URL.self, forKey: .url)
+        score = try container.decodeIfPresent(Int.self, forKey: .score)
+        title = try container.decodeIfPresent(String.self, forKey: .title)
+        parts = try container.decodeIfPresent([Int].self, forKey: .parts)
+        descendants = try container.decodeIfPresent(Int.self, forKey: .descendants)
+    }
 }
